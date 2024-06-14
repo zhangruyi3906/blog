6ad2ab70da14792b03f2b4865a5499a33aa47c12
zhangruyi3906 <3023208132@qq.com>
2024-06-14 19:58:53 +0800

    修改全文

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,6 +1,22 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const Sidebar = sidebar({
+//开源项目
+    "/project/": [
+        {
+            text: "项目", // 分组显示名称
+            icon: "community", // 分组显示图标
+            children: "structure", // 分组下的子分组或页面 structure代表自动获取
+            // collapsible: false,
+        },
+    ],
+	"/java/": [
+        {
+            text: "Java", // 分组显示名称
+            icon: "book", // 分组显示图标
+            children: "structure", // 分组下的子分组或页面 structure代表自动获取
+        },
+    ],
   // 代码笔记的侧边栏
   "/docs/": [
     {
@@ -102,7 +118,13 @@ export const Sidebar = sidebar({
       ],
     },
   ],
-
+"/interview/": [
+        {
+            text: "面试题", // 分组显示名称
+            icon: "java", // 分组显示图标
+            children: "structure", // 分组下的子分组或页面 structure代表自动获取
+        },
+    ],
   // 开源项目的侧边栏
   "/autodir/": "structure"
 });
